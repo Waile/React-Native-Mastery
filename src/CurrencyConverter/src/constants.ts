1c83c034e4e1a9e784a73576a8926a920f78d7ff
Waile Naeem <wailenaeem22@gmail.com>
2023-08-07 22:46:04 +0500

    currency conerter app

---
+++ src/CurrencyConverter/src/constants.ts
@@ -0,0 +1,56 @@
+export const currencyByRupee: Currency[] = [
+    {
+        name: 'DOLLAR',
+        value: 0.0035,
+        flag: '🇺🇸',
+        symbol: '$',
+    },
+    {
+        name: 'EURO',
+        value: 0.0032,
+        flag: '🇪🇺',
+        symbol: '€',
+    },
+    {
+        name: 'POUND',
+        value: 0.0028,
+        flag: '🇬🇧',
+        symbol: '£',
+    },
+    {
+        name: 'RUBEL',
+        value: 0.34,
+        flag: '🇷🇺',
+        symbol: '₽',
+    },
+    {
+        name: 'AUS DOLLAR',
+        value: 0.0054,
+        flag: '🇦🇺',
+        symbol: 'A$',
+    },
+    {
+        name: 'CAN DOLLAR',
+        value: 0.0047,
+        flag: '🇨🇦',
+        symbol: 'C$',
+    },
+    {
+        name: 'YEN',
+        value: 0.50,
+        flag: '🇯🇵',
+        symbol: '¥',
+    },
+    {
+        name: 'DINAR',
+        value: 0.0011,
+        flag: '🇰🇼',
+        symbol: 'KD',
+    },
+    {
+        name: 'BITCOIN',
+        value: 0.000000122,
+        flag: '🎰',
+        symbol: '₿',
+    },
+];
