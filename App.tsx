2634db65073187eda5f491da1981bd49e5cd75f7
Waile Naeem <wailenaeem22@gmail.com>
2023-08-09 23:38:38 +0500

    tic tac toe

--- App.tsx
+++ App.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 
 import CurrencyConverter from './src/CurrencyConverter/src/CurrencyConverter';
-
+import TicTacToe from './src/Tic-Tac-Toe/src/TicTacToe';
 
 function App(): JSX.Element {
-
-
-  return <CurrencyConverter />
+  return <TicTacToe />;
 }
-
-
 
 export default App;
